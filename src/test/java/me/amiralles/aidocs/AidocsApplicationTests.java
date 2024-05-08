1049324a949a3481b5de3bf135e58e2f9535da2d
amiralle <anmiralles@yahoo.es>
2024-05-08 13:20:34 +0200

    first commit

---
+++ src/test/java/me/amiralles/aidocs/AidocsApplicationTests.java
@@ -0,0 +1,13 @@
+package me.amiralles.aidocs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AidocsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
